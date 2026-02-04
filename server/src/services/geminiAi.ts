2814de48d7ef1bb9db276eeca6bc6ccb4723c674
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-02-04 13:15:22 +0530

    Feat: Implemented the Ai with strong error handling and understanding

---
+++ server/src/services/geminiAi.ts
@@ -0,0 +1,34 @@
+// The Professor: where i created the gemini ai service. It talks to Google Gemini.
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const Gemini_Api_Key = process.env.GEMINI_API_KEY
+if (!Gemini_Api_Key) {
+    throw new Error("ERROR: GEMINI API KEY is missing in .env file")
+}
+
+const geminiIntialized = new GoogleGenerativeAI(Gemini_Api_Key)
+
+export default async function geminiAI(prompt: string) {
+    try {
+        const model = geminiIntialized.getGenerativeModel({ model: "gemini-1.5-flash" })
+        const result = await model.generateContent(prompt)
+            
+        const response = result.response
+
+        if (!response || !response.candidates || response.candidates.length === 0) {
+            throw new Error("AI returned no answers");
+        }//Candidates are the responses from gemini
+        console.log("Recieved response from Gemini: ", response.text())
+
+        return response.text()
+
+    } catch (error: any) {
+        console.error("AI Error:", error.message || error);
+        throw new Error(`Failed to generate response from AI ${error.message || "Unknown Error"}`)
+    }
+
+}
