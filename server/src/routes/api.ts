ea56bbebd6844e3b0a65c6335f707dafda3a9f12
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-02-04 15:56:24 +0530

    Added the route for ai

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express"
+import generateResponse from "../services/geminiAi"
 const router = Router()
 
 router.post('/chat', async (req: Request, res: Response) => {
@@ -10,10 +11,14 @@ router.post('/chat', async (req: Request, res: Response) => {
         }
         console.log("Message from client: ", prompt);
 
+        const response = await generateResponse(prompt)
 
-        
+        res.status(200).json({ response })
         
     } catch (error) {
-        
+        console.error("Error in chat route:", error);
+        res.status(500).json({ error: "AI Service Failed" })
     }
 })
+
+export default router
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import apiRouter from './routes/api'
 
 dotenv.config()
 
@@ -9,6 +10,8 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+app.use('/api', apiRouter)
 
 app.get('/', (req, res) => {
     res.send('The Professor API is Live Now')
