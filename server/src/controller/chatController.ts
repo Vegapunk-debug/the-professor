59cd0cbf6c1775f8005163835b123364fb4b87fa
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-02-08 14:17:16 +0530

    Added the chat logic in controller file

---
+++ server/src/controller/chatController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express"
+import generateResponse from "../services/geminiAi"
+
+export const handleChat = async (req: Request, res: Response) => {
+    try {
+        const { prompt } = req.body
+
+        if (!prompt) {
+            return res.status(400).json({ error: "Message is required" })
+        }
+        console.log("Message from client: ", prompt);
+
+        const response = await generateResponse(prompt)
+
+        res.status(200).json({ response })
+        
+    } catch (error) {
+        console.error("Error in chat route:", error);
+        res.status(500).json({ error: "AI Service Failed" })
+    }
+}
--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,24 +1,7 @@
-import { Router, Request, Response } from "express"
-import generateResponse from "../services/geminiAi"
+import { Router } from "express"
+import { handleChat } from "../controller/chatController"
 const router = Router()
 
-router.post('/chat', async (req: Request, res: Response) => {
-    try {
-        const { prompt } = req.body
-
-        if (!prompt) {
-            return res.status(400).json({ error: "Message is required" })
-        }
-        console.log("Message from client: ", prompt);
-
-        const response = await generateResponse(prompt)
-
-        res.status(200).json({ response })
-        
-    } catch (error) {
-        console.error("Error in chat route:", error);
-        res.status(500).json({ error: "AI Service Failed" })
-    }
-})
+router.post('/chat', handleChat)
 
 export default router
