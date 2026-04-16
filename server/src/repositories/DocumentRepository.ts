048e7dbf8945d7aedae3d801bf211748099d4dee
Rohit Nair P <rohitnairmuttathethu@gmail.com>
2026-04-16 14:12:33 +0530

    feat: implement DocumentRepository

---
+++ server/src/repositories/DocumentRepository.ts
@@ -0,0 +1,13 @@
+import DocumentModel, { IDocument } from "../models/Document"
+
+export class DocumentRepository {
+    async create(documentData: Partial<IDocument>): Promise<IDocument> {
+        return await DocumentModel.create(documentData)
+    }
+
+    async findByUserId(userId: string): Promise<IDocument[]> {
+        return await DocumentModel.find({ user_id: userId })
+            .select("file_name createdAt _id")
+            .sort({ createdAt: -1 })
+    }
+}
